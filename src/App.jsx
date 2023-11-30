0af145f8f780cdfb584f817a49606f4d4fc30f89
Yash1Hingu <yash23hingu@gmail.com>
2023-11-30 21:40:03 +0530

    Added NoProjectComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex">
       <ProjectSidebar/>
-      <NewProject/>
+      <NoProjectSelected/>
     </main>
   );
 }
