11e0f40c9c463be53ad1acc481eb1dfd309c928f
Yash1Hingu <yash23hingu@gmail.com>
2023-11-30 20:57:34 +0530

    Added NewProject component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import NewProject from "./components/NewProject";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
   return (
-    <>
+    <main className="h-screen my-8 flex">
       <ProjectSidebar/>
-    </>
+      <NewProject/>
+    </main>
   );
 }
 
